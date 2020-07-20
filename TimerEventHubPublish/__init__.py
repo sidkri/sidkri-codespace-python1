b1511710044a3f311958ecb319bcd5ab19257173
Sid Krishna <sidkri@users.noreply.github.com>
2020-07-20 06:06:08 +0000

    Fix

--- TimerEventHubPublish/__init__.py
+++ TimerEventHubPublish/__init__.py
@@ -6,9 +6,11 @@ import azure.functions as func
 def main(timer: func.TimerRequest) -> str:
     if timer.past_due:
         logging.info('The timer is past due!')
+    msgs = list()
     for i in range(1,10):
         utc_timestamp = datetime.datetime.utcnow().replace(
             tzinfo=datetime.timezone.utc).isoformat()
         msg = 'Message created at: {}'.format(utc_timestamp)
         logging.info('>>>>> Publishing to Event Hub Message: %s', msg)
-    return msg
+        msgs.append(msg)
+    return msgs
